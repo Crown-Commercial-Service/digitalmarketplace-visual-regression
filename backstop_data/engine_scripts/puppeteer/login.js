c89f021fca18298deb92e09f905042ff1d4d8e6f
Laurence de Bruxelles <laurence.debruxelles@digital.cabinet-office.gov.uk>
2020-02-12 14:28:15 +0000

    Remove support for digitalmarketplace-frontend-toolkit login button

--- backstop_data/engine_scripts/puppeteer/login.js
+++ backstop_data/engine_scripts/puppeteer/login.js
@@ -23,7 +23,7 @@ module.exports = async (page, scenario) => {
 
   await Promise.all([
     page.waitForNavigation(),
-    page.click('.govuk-button, .button-save'), //should redirect you to desired page
+    page.click('.govuk-button'), //should redirect you to desired page
   ]);
 
   return;
