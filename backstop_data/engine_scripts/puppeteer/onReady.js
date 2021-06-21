e4ef438b76b372a4cd3411515482ef076c1762af
Alex Smith <alex.me.smith@gmail.com>
2021-06-21 14:32:22 +0100

    Revert "Bump backstopjs to 5.3.2"

--- backstop_data/engine_scripts/puppeteer/onReady.js
+++ backstop_data/engine_scripts/puppeteer/onReady.js
@@ -4,9 +4,5 @@ module.exports = async (page, scenario, vp) => {
   await require('./submitFormHelper')(page, scenario);
   await require('./clickAndHoverHelper')(page, scenario);
 
-  // Wait for fonts to load and scroll to the top of the page before taking a screenshot
-  await page.evaluateHandle('document.fonts.ready');
-  await page.evaluate(() => window.scrollTo(0, 0))
-
   // add more ready handlers here...
 };
