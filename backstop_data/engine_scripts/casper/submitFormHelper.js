fb2a03118611462db3c77db77a02ae38d3c4e02e
Benjamin Gill <benjamin.gill@digital.cabinet-office.gov.uk>
2021-07-29 12:02:13 +0100

    Remove old PhantomJS engine scripts
    In 70d9d115d3b27a7cb245d47b5c843a20bf69dd50, we switched over to using Chrome/Puppeteer for our tests instead of PhantomJS/Casper. We've not used the Casper scripts in a very long time, so we can safely remove them.

--- backstop_data/engine_scripts/casper/submitFormHelper.js
+++
@@ -1,7 +0,0 @@
-//well, this could be made more generic couldn't it?
-module.exports = function(casper, scenario) {
-  //just submit the empty form
-  casper.thenEvaluate(function() {
-    document.querySelector('main form').submit(); // ignore forms in header and footer
-  });
-};
